37d0e7bf8904c9bf3c8f71e686e4a81aa44d4752
Diego Melo <diego-melo@uni9.edu.br>
2024-05-10 18:11:00 -0300

    desafio003

---
+++ Python/desafio003.py
@@ -0,0 +1,9 @@
+#Crie um script python que leia dois números e tente mostrar a soma entre eles.
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+soma = n1 + n2
+
+
+print('A soma entre {} e {} é {}'.format(n1,n2,soma))
